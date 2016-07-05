262cc4d4ab511d58664dc26978c01829d4d5d469
Albert Lee <snowartpeople@gmail.com>
2016-07-05 17:43:51 +0300

    turn mockup to django template

--- fortytwo_test_task/urls.py
+++ fortytwo_test_task/urls.py
@@ -1,13 +1,11 @@
 from django.conf.urls import patterns, include, url
+from django.views.generic import TemplateView
 
 from django.contrib import admin
 admin.autodiscover()
 
-urlpatterns = patterns(
-    '',
-    # Examples:
-    # url(r'^$', 'fortytwo_test_task.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
+urlpatterns = [
 
     url(r'^admin/', include(admin.site.urls)),
-)
+    url(r'^template/', TemplateView.as_view(template_name='contact_page.html'), name='template'),
+]
