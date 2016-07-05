ec22673381e6fde90c8fbc96bcb1dc9a13c3665f
Albert Lee <snowartpeople@gmail.com>
2016-07-05 20:40:30 +0300

    ticket:1 fixed urls.py

--- fortytwo_test_task/urls.py
+++ fortytwo_test_task/urls.py
@@ -1,3 +1,4 @@
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.conf.urls import include, url
 from django.contrib import admin
 
@@ -8,3 +9,4 @@ urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'^$', 'apps.hello.views.contact', name='contact_page_view'),
 ]
+urlpatterns += staticfiles_urlpatterns()
