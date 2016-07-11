842a8730fc1d85297fa9ea36cba53c3f9220980d
Albert Lee <snowartpeople@gmail.com>
2016-07-11 13:52:44 +0300

    ticket:3 added template for requests

--- fortytwo_test_task/urls.py
+++ fortytwo_test_task/urls.py
@@ -1,6 +1,7 @@
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.views.generic import TemplateView
 
 admin.autodiscover()
 
@@ -8,5 +9,6 @@ urlpatterns = [
 
     url(r'^admin/', include(admin.site.urls)),
     url(r'^$', 'apps.hello.views.contact', name='contact_page_view'),
+    url(r'^requests$', TemplateView.as_view(template_name='requests_page.html'), name='requests_page_view'),
 ]
 urlpatterns += staticfiles_urlpatterns()
