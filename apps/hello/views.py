f3c471bc993cd105744283d371e003bc1e63e72d
Albert Lee <snowartpeople@gmail.com>
2016-07-11 14:24:00 +0300

    ticket:3 added test & view for hard-coded data on requests page

--- apps/hello/views.py
+++ apps/hello/views.py
@@ -5,3 +5,8 @@ from django.shortcuts import render
 def contact(request):
     person = Person.objects.first()
     return render(request, 'contact_page.html', {'person': person})
+
+
+def requests(request):
+    context = {'method': "'GET'", 'path': "'/'", 'time': "'1989-12-22'"}
+    return render(request, 'requests_page.html', context)
--- fortytwo_test_task/urls.py
+++ fortytwo_test_task/urls.py
@@ -1,7 +1,6 @@
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.conf.urls import include, url
 from django.contrib import admin
-from django.views.generic import TemplateView
 
 admin.autodiscover()
 
@@ -9,10 +8,6 @@ urlpatterns = [
 
     url(r'^admin/', include(admin.site.urls)),
     url(r'^$', 'apps.hello.views.contact', name='contact_page_view'),
-    url(
-        r'^requests$',
-        TemplateView.as_view(template_name='requests_page.html'),
-        name='requests_page_view'
-    ),
+    url(r'^requests$', 'apps.hello.views.requests', name='requests_page_view'),
 ]
 urlpatterns += staticfiles_urlpatterns()
